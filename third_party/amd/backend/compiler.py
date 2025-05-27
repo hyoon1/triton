f0d405ad797fb74020dcb8e715a6bbf8e1493974
Paul Trojahn <paul.trojahn@amd.com>
2025-05-27 14:27:39 +0200

    Cleanup debugging helper

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -228,7 +228,6 @@ class HIPBackend(BaseBackend):
         passes.ttgpuir.add_remove_layout_conversions(pm)
         amd.passes.ttgpuir.add_optimize_epilogue(pm)
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
-        # XXX
         amd.passes.ttgpuir.add_hoist_layout_conversions(pm)
 
         global_prefetch = int(os.getenv("TRITON_HIP_GLOBAL_PREFETCH", "0"))
